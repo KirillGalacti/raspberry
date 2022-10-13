eb0090790fdea33950bcf3d1b863b1d9068e3270
KirillGalacti <89073958+KirillGalacti@users.noreply.github.com>
2022-10-13 03:56:08 +0300

    Add files via upload

--- program/main.py
+++ program/main.py
@@ -1,23 +1,33 @@
-from PyQt6.QtWidgets import QApplication, QWidget
-
+from PyQt6.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from ui import Ui_Form
 import sys
-from ui import Ui_Form # Только для доступа к аргументам командной строки
 
-# Приложению нужен один (и только один) экземпляр QApplication.
-# Передаём sys.argv, чтобы разрешить аргументы командной строки для приложения.
-# Если не будете использовать аргументы командной строки, QApplication([]) тоже работает
-app = QApplication(sys.argv)
+class MainWindow:
+    def __init__(self):
+        self.window = QWidget()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self.window)
+        
+    
+    def show(self):
+        self.window.show()
 
-# Создаём виджет Qt — окно.
-window = QWidget()
-ui = Ui_Form()
-ui.setupUi(window)
-
-window.show()  # Важно: окно по умолчанию скрыто.
-
-# Запускаем цикл событий.
-app.exec()
+        
 
 
-# Приложение не доберётся сюда, пока вы не выйдете и цикл
-# событий не остановится.
+if __name__ == '__main__':
+    # Приложению нужен один (и только один) экземпляр QApplication.
+    # Передаём sys.argv, чтобы разрешить аргументы командной строки для приложения.
+    # Если не будете использовать аргументы командной строки, QApplication([]) тоже работает
+    app = QApplication([])
+    # Создаём виджет Qt — окно.
+    window = MainWindow()
+    window.show()  
+    # Важно: окно по умолчанию скрыто.
+    # Запускаем цикл событий.
+    sys.exit(app.exec_())  
+    # Приложение не доберётся сюда, пока вы не выйдете и цикл
+    # событий не остановится.
--- program/ui.py
+++ program/ui.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'test.ui'
+## Form generated from reading UI file 'ui.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -17,7 +17,7 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(901, 818)
+        Form.resize(844, 767)
         Form.setStyleSheet(u"QPushButton{\n"
 "	background-color: #0d6efd;\n"
 "	color: #fff;\n"
@@ -38,85 +38,119 @@ class Ui_Form(object):
 "	font-weight: bold;\n"
 "	text-align: center;\n"
 "}\n"
-"")
-        self.gridLayout = QGridLayout()
+"\n"
+"QWidget{\n"
+"	qlineargradient(spread:pad, x1:0.722, y1:0.255818, x2:0.1815, y2:0.858, stop:0 rgba(145, 234, 228, 255), stop:1 rgba(127, 127, 213, 255))\n"
+"}")
+        self.gridLayout = QGridLayout(Form)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.scrollArea = QScrollArea()
+        self.stackedWidget = QStackedWidget(Form)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.page = QWidget()
+        self.page.setObjectName(u"page")
+        self.gridLayout_2 = QGridLayout(self.page)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.scrollArea = QScrollArea(self.page)
         self.scrollArea.setObjectName(u"scrollArea")
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 864, 1065))
-        self.gridLayout_2 = QGridLayout(self.scrollAreaWidgetContents)
-        self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.pushButton_10 = QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_10.setObjectName(u"pushButton_10")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 789, 1146))
+        self.gridLayout_3 = QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.pushButton_5 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_5.setObjectName(u"pushButton_5")
 
-        self.gridLayout_2.addWidget(self.pushButton_10, 13, 0, 1, 1)
-
-        self.pushButton = QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton.setObjectName(u"pushButton")
-
-        self.gridLayout_2.addWidget(self.pushButton, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.pushButton_5, 1, 0, 1, 1)
 
         self.pushButton_2 = QPushButton(self.scrollAreaWidgetContents)
         self.pushButton_2.setObjectName(u"pushButton_2")
 
-        self.gridLayout_2.addWidget(self.pushButton_2, 9, 0, 1, 1)
-
-        self.pushButton_8 = QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_8.setObjectName(u"pushButton_8")
-
-        self.gridLayout_2.addWidget(self.pushButton_8, 8, 0, 1, 1)
-
-        self.pushButton_7 = QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_7.setObjectName(u"pushButton_7")
-
-        self.gridLayout_2.addWidget(self.pushButton_7, 4, 0, 1, 1)
-
-        self.pushButton_9 = QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_9.setObjectName(u"pushButton_9")
-
-        self.gridLayout_2.addWidget(self.pushButton_9, 6, 0, 1, 1)
-
-        self.pushButton_11 = QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_11.setObjectName(u"pushButton_11")
-
-        self.gridLayout_2.addWidget(self.pushButton_11, 12, 0, 1, 1)
-
-        self.pushButton_4 = QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_4.setObjectName(u"pushButton_4")
-
-        self.gridLayout_2.addWidget(self.pushButton_4, 2, 0, 1, 1)
-
-        self.pushButton_5 = QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_5.setObjectName(u"pushButton_5")
-
-        self.gridLayout_2.addWidget(self.pushButton_5, 3, 0, 1, 1)
-
-        self.pushButton_3 = QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-
-        self.gridLayout_2.addWidget(self.pushButton_3, 1, 0, 1, 1)
-
-        self.pushButton_13 = QPushButton(self.scrollAreaWidgetContents)
-        self.pushButton_13.setObjectName(u"pushButton_13")
-
-        self.gridLayout_2.addWidget(self.pushButton_13, 10, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.pushButton_2, 13, 0, 1, 1)
 
         self.pushButton_12 = QPushButton(self.scrollAreaWidgetContents)
         self.pushButton_12.setObjectName(u"pushButton_12")
 
-        self.gridLayout_2.addWidget(self.pushButton_12, 11, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.pushButton_12, 8, 0, 1, 1)
+
+        self.pushButton_9 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+
+        self.gridLayout_3.addWidget(self.pushButton_9, 9, 0, 1, 1)
+
+        self.pushButton_11 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_11.setObjectName(u"pushButton_11")
+
+        self.gridLayout_3.addWidget(self.pushButton_11, 5, 0, 1, 1)
+
+        self.pushButton_3 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+
+        self.gridLayout_3.addWidget(self.pushButton_3, 3, 0, 1, 1)
+
+        self.pushButton_8 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_8.setObjectName(u"pushButton_8")
+
+        self.gridLayout_3.addWidget(self.pushButton_8, 10, 0, 1, 1)
+
+        self.pushButton = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.gridLayout_3.addWidget(self.pushButton, 0, 0, 1, 1)
+
+        self.pushButton_4 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+
+        self.gridLayout_3.addWidget(self.pushButton_4, 14, 0, 1, 1)
+
+        self.pushButton_7 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_7.setObjectName(u"pushButton_7")
+
+        self.gridLayout_3.addWidget(self.pushButton_7, 11, 0, 1, 1)
+
+        self.pushButton_10 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_10.setObjectName(u"pushButton_10")
+
+        self.gridLayout_3.addWidget(self.pushButton_10, 4, 0, 1, 1)
 
         self.pushButton_6 = QPushButton(self.scrollAreaWidgetContents)
         self.pushButton_6.setObjectName(u"pushButton_6")
 
-        self.gridLayout_2.addWidget(self.pushButton_6, 7, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.pushButton_6, 12, 0, 1, 1)
+
+        self.pushButton_13 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_13.setObjectName(u"pushButton_13")
+
+        self.gridLayout_3.addWidget(self.pushButton_13, 6, 0, 1, 1)
+
+        self.pushButton_14 = QPushButton(self.scrollAreaWidgetContents)
+        self.pushButton_14.setObjectName(u"pushButton_14")
+        self.pushButton_14.setStyleSheet(u"")
+
+        self.gridLayout_3.addWidget(self.pushButton_14, 7, 0, 1, 1)
 
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
 
-        self.gridLayout.addWidget(self.scrollArea, 1, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.scrollArea, 0, 0, 1, 1)
+
+        self.stackedWidget.addWidget(self.page)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.gridLayout_4 = QGridLayout(self.page_2)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.pushButton_15 = QPushButton(self.page_2)
+        self.pushButton_15.setObjectName(u"pushButton_15")
+
+        self.gridLayout_4.addWidget(self.pushButton_15, 0, 0, 1, 1)
+
+        self.pushButton_16 = QPushButton(self.page_2)
+        self.pushButton_16.setObjectName(u"pushButton_16")
+
+        self.gridLayout_4.addWidget(self.pushButton_16, 1, 0, 1, 1)
+
+        self.stackedWidget.addWidget(self.page_2)
+
+        self.gridLayout.addWidget(self.stackedWidget, 1, 0, 1, 1)
 
         self.label = QLabel(Form)
         self.label.setObjectName(u"label")
@@ -127,24 +161,30 @@ class Ui_Form(object):
 
         self.retranslateUi(Form)
 
+        self.stackedWidget.setCurrentIndex(1)
+
+
         QMetaObject.connectSlotsByName(Form)
     # setupUi
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        self.pushButton.setText(QCoreApplication.translate("Form", u"PushButton", None))
-        self.pushButton_2.setText(QCoreApplication.translate("Form", u"PushButton", None))
-        self.pushButton_3.setText(QCoreApplication.translate("Form", u"PushButton", None))
-        self.pushButton_4.setText(QCoreApplication.translate("Form", u"PushButton", None))
         self.pushButton_5.setText(QCoreApplication.translate("Form", u"PushButton", None))
-        self.pushButton_6.setText(QCoreApplication.translate("Form", u"PushButton", None))
-        self.pushButton_7.setText(QCoreApplication.translate("Form", u"PushButton", None))
-        self.pushButton_8.setText(QCoreApplication.translate("Form", u"PushButton", None))
-        self.pushButton_9.setText(QCoreApplication.translate("Form", u"PushButton", None))
-        self.pushButton_10.setText(QCoreApplication.translate("Form", u"PushButton", None))
-        self.pushButton_11.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Form", u"PushButton", None))
         self.pushButton_12.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_9.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_11.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_8.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_7.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_10.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_6.setText(QCoreApplication.translate("Form", u"PushButton", None))
         self.pushButton_13.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_14.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_15.setText(QCoreApplication.translate("Form", u"PushButton", None))
+        self.pushButton_16.setText(QCoreApplication.translate("Form", u"PushButton", None))
         self.label.setText(QCoreApplication.translate("Form", u"\u041b\u0430\u0431\u043e\u0440\u0430\u0442\u043e\u0440\u043d\u044b\u0439 \u043f\u0440\u0430\u043a\u0442\u0438\u043a\u0443\u043c", None))
     # retranslateUi
 
