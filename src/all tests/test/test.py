903a1be1e4009af1e3ea3faf60cf8bbc3232600f
KirillGalacti <89073958+KirillGalacti@users.noreply.github.com>
2022-12-06 20:50:27 +0700

    Add files via upload

--- src/all tests/test/test.py
+++ src/all tests/test/test.py
@@ -5,28 +5,41 @@ import RPi.GPIO as IO
 IO.setmode(IO.BCM)
 IO.setup(5,IO.IN)
 IO.setup(6,IO.IN)
-IO.setup(13,IO.IN)
 
 while True
-def buttonPressed(channel):
+def buttonPressed1(channel):
     """This function should be called when GPIO 7 has a rising edge event"""
-    global enable
+    global enabled1
     print("Button was pressed")
-    enable.set(enable.get() + 1)
+    enabled1.set(True)
+def buttonPressed2(channel):
+    """This function should be called when GPIO 7 has a rising edge event"""
+    global enabled2
+    print("Button was pressed")
+    enabled2.set(True)
 
 """Register the callback"""
-IO.add_event_detect(5, IO.RISING, callback=buttonPressed)
+IO.add_event_detect(5, IO.RISING, callback=buttonPressed1)
+IO.add_event_detect(6, IO.RISING, callback=buttonPressed2)
 
 root = Tk()
 root.title("TEST")
 root.geometry("250x200")
 
-enabled = IntVar()
+enabled1 = BooleanVar()
   
-enabled_checkbutton = ttk.Checkbutton(text="Включить", variable=enabled)
-enabled_checkbutton.pack(padx=6, pady=6, anchor=NW)
+enabled_checkbutton1 = ttk.Checkbutton(text="Включить", variable=enabled1)
+enabled_checkbutton1.pack(padx=6, pady=6, anchor=NW)
   
-enabled_label = ttk.Label(textvariable=enabled)
-enabled_label.pack(padx=6, pady=6, anchor=NW)
+enabled_label1 = ttk.Label(textvariable=enabled1)
+enabled_label1.pack(padx=6, pady=6, anchor=NW)
+
+enabled2 = BooleanVar()
+  
+enabled_checkbutton2 = ttk.Checkbutton(text="Включить", variable=enabled2)
+enabled_checkbutton2.pack(padx=6, pady=6, anchor=NW)
+  
+enabled_label2 = ttk.Label(textvariable=enabled2)
+enabled_label2.pack(padx=6, pady=6, anchor=NW)
  
 root.mainloop()
